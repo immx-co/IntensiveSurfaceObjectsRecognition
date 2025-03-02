f83e6e7137ae531b3face9d08383fe62674489a8
immx <immx@DESKTOP-R1VTDBF>
2025-03-02 11:47:26 +0300

    added save configuration

---
+++ ObjectsRecognitionUI/ClassLibrary/ApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary;
+
+public class AppSettings
+{
+    public ConnectionStringsConfig ConnectionStrings { get; set; }
+}
+
+public class ConnectionStringsConfig
+{
+    public string dbStringConnection { get; set; }
+    public string srsStringConnection { get; set; }
+}
--- ObjectsRecognitionUI/ObjectsRecognitionUI/ViewModels/ConfigurationViewModel.cs
+++ ObjectsRecognitionUI/ObjectsRecognitionUI/ViewModels/ConfigurationViewModel.cs
@@ -1,6 +1,13 @@
-﻿using ReactiveUI;
+﻿using Microsoft.Extensions.Configuration;
+using ReactiveUI;
 using System;
+using System.Reactive;
+using System.Text.Json;
 using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using ClassLibrary;
+using MsBox.Avalonia;
 
 namespace ObjectsRecognitionUI.ViewModels
 {
@@ -10,12 +17,21 @@ namespace ObjectsRecognitionUI.ViewModels
         private string _connectionString;
 
         private string _url;
+
+        private readonly IConfiguration _configuration;
         #endregion
+
+        #region View Model Settings
         public IScreen HostScreen { get; }
 
         public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
 
         public CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        #endregion
+
+        #region Commands
+        public ReactiveCommand<Unit, Unit> SaveConfigCommand { get; }
+        #endregion
 
         public string ConnectionString
         {
@@ -29,9 +45,51 @@ namespace ObjectsRecognitionUI.ViewModels
             set => this.RaiseAndSetIfChanged(ref _url, value);
         }
 
-        public ConfigurationViewModel(IScreen screen)
+        public ConfigurationViewModel(IScreen screen, IConfiguration configuration)
         {
             HostScreen = screen;
+            _configuration = configuration;
+
+            ConnectionString = _configuration.GetConnectionString("dbStringConnection");
+            Url = _configuration.GetConnectionString("srsStringConnection");
+
+            SaveConfigCommand = ReactiveCommand.CreateFromTask(SaveConfig);
+        }
+
+        private async Task SaveConfig()
+        {
+            try
+            {
+                var appSettingsPath = Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\", "appsettings.json");
+
+                var json = await File.ReadAllTextAsync(appSettingsPath);
+
+                var appSettings = JsonSerializer.Deserialize<AppSettings>(json);
+
+                appSettings.ConnectionStrings.dbStringConnection = ConnectionString;
+                appSettings.ConnectionStrings.srsStringConnection = Url;
+
+                var updatedJson = JsonSerializer.Serialize(appSettings, new JsonSerializerOptions { WriteIndented = true });
+
+                await File.WriteAllTextAsync(appSettingsPath, updatedJson);
+
+                ShowMessageBox("Success", $"Конфигурация успешно сохранена!");
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBox("Failed", "Возникла ошибка при сохранении конфигурации.");
+            }
+        }
+
+        /// <summary>
+        /// Показывает всплывающее сообщение.
+        /// </summary>
+        /// <param name="caption">Заголовок сообщения.</param>
+        /// <param name="message">Сообщение пользователю.</param>
+        public void ShowMessageBox(string caption, string message)
+        {
+            var messageBoxStandardWindow = MessageBoxManager.GetMessageBoxStandard(caption, message);
+            messageBoxStandardWindow.ShowAsync();
         }
     }
 }
